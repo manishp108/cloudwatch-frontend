5095e482b90f71dc3040fd690507095dd3f41871
manish108 <manish.developer18@gmail.com>
2026-01-30 16:30:49 +0530

    register all components and pop-up modals in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,59 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatButtonModule } from "@angular/material/button";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { MessagesComponent } from "./messages/messages.component";
-import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
+import { UserService } from "./services/user.service";
 import { AppRoutingModule } from "./app-routing.module";
-import { FeedsComponent } from "./feeds/feeds.component"; 
-import { SharedService } from "./services/shared.service"; 
-import { FeedService } from "./services/feed.service";
+import { FormsModule } from "@angular/forms";
+import { TenxAppNavigationComponent } from "./tenx-app-navigation/tenx-app-navigation.component";
+import { FeedsComponent } from "./feeds/feeds.component";
+import { NotificationsPopupComponent } from "./modals/notifications-popup/notifications-popup.component";
+import { EditProfilePopUpComponent } from "./modals/edit-profile-pop-up/edit-profile-pop-up.component";
+import { MessagesPopupComponent } from "./modals/messages-popup/messages-popup.component";
+import { LoginPopupComponent } from "./modals/login-popup/login-popup.component";
+import { LikesPopupComponent } from "./modals/likes-popup/likes-popup.component";
+import { CommentsPopupComponent } from "./modals/comments-popup/comments-popup.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { ChatComponent } from "./chat/chat.component";
+import { EditProfileComponent } from "./edit-profile/edit-profile.component";
+import { AboutComponent } from "./about/about.component";
+import { VideoDisplayComponent } from "./video-display/video-display.component";
+// import { LoginComponent } from "./login/login.component";
+import { CreateComponent } from "./create/create.component";
+import { NotificationsComponent } from "./notifications/notifications.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { MobileNavigationComponent } from "./mobile-navigation/mobile-navigation.component";
+import { VideoPlayerComponent } from "./video-player/video-player.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    TenxAppNavigationComponent,
+    FeedsComponent,
     MessagesComponent,
-    FeedsComponent, 
+    CommentsPopupComponent,
+    NotificationsPopupComponent,
+    EditProfilePopUpComponent,
+    MessagesPopupComponent,
+    LoginPopupComponent,
+    LikesPopupComponent,
+    ChatComponent,
+    EditProfileComponent,
+    AboutComponent,
+    VideoDisplayComponent,
+    // LoginComponent,
+    CreateComponent,
+    NotificationsComponent,
+    ProfileComponent,
+    MobileNavigationComponent,
+    VideoPlayerComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,11 +64,7 @@ import { FeedService } from "./services/feed.service";
     MatProgressBarModule,
     AppRoutingModule,
   ],
-  providers: [
-    SharedService, 
-    FeedService, 
-    provideHttpClient(withInterceptorsFromDi())
-  ],
+  providers: [UserService, provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
