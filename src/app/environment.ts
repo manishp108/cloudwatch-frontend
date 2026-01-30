ad4e36152b5bd991a96bd6dc3a58f5f19ad395c6
manish108 <manish.developer18@gmail.com>
2026-01-30 16:45:05 +0530

    Set primary API endpoint to cloudwatch-api-app and enable production mode

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,10 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
+  production: true,
+  apiUrl: 'https://cloudwatch-api-app.azurewebsites.net/api',
   GOOGLE_CLIENT_ID:
     "398443498935-09d0d4b8arlas27lmb02k47f0l7i44ul.apps.googleusercontent.com",
+  // apiUrl: 'https://snappservice.azurewebsites.net/api',
+  // apiUrl: 'https://tenxukwest.azurewebsites.net/api',
+  // apiUrl: 'https://tenxapi101.azurewebsites.net/api',
+  // nbapiUrl: 'https://backendsocial.azurewebsites.net/api', // Your production API URL for development
 };
